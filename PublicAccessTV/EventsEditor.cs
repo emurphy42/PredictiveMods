c50d49c026c3704cc252f6d7671963b29cafa4de
emurphy42 <emurphy42@gmail.com>
2024-04-12 19:58:41 -0700

    PublicAccessTV: add original source code

---
+++ PublicAccessTV/EventsEditor.cs
@@ -0,0 +1,44 @@
+using StardewModdingAPI;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace PublicAccessTV
+{
+	internal class EventsEditor : IAssetEditor
+	{
+		protected static IModHelper Helper => ModEntry.Instance.Helper;
+		protected static IMonitor Monitor => ModEntry.Instance.Monitor;
+		protected static ModConfig Config => ModConfig.Instance;
+
+		public bool CanEdit<_T> (IAssetInfo asset)
+		{
+			if (Config.BypassFriendships)
+				return false;
+
+			return asset.AssetNameEquals ($"Data\\Events\\{GarbageChannel.EventMap}") ||
+				asset.AssetNameEquals ($"Data\\Events\\{TrainsChannel.EventMap}");
+		}
+
+		public void Edit<_T> (IAssetData asset)
+		{
+			var data = asset.AsDictionary<string, string> ().Data;
+
+			if (asset.AssetNameEquals ($"Data\\Events\\{GarbageChannel.EventMap}"))
+				applyEvents ("garbage", data, GarbageChannel.Events);
+
+			if (asset.AssetNameEquals ($"Data\\Events\\{TrainsChannel.EventMap}"))
+				applyEvents ("trains", data, TrainsChannel.Events);
+		}
+
+		private void applyEvents (string module, IDictionary<string, string> to,
+			IDictionary<string, string> from)
+		{
+			foreach (string key in from.Keys.ToList ())
+			{
+				to[key] = from[key] = Regex.Replace (from[key], @"\{\{([^}]+)\}\}",
+					(match) => Helper.Translation.Get ($"{module}.event.{match.Groups[1]}"));
+			}
+		}
+	}
+}
